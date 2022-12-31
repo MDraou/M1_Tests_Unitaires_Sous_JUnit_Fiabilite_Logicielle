cdf1a05d4846866099416e2ae8e2a3db76cc4a74
Jean-Marc Talbot <jean-marc.talbot@lis-lab.fr>
2022-12-31 14:46:52 +0100

    add src

---
+++ src/main/java/complex/Complex.java
@@ -0,0 +1,62 @@
+package complex;
+
+@SuppressWarnings("PMD.JUnitAssertionsShouldIncludeMessage")
+public class Complex {
+    float realPart = 0;
+    float imaginaryPart = 0;
+
+    public Complex() {
+        super();
+    }
+
+    public Complex(float a, float b) {
+        this.realPart = a;
+        this.imaginaryPart = b;
+    }
+
+
+    public boolean isZero() {
+        return ((this.realPart == 0) && (this.imaginaryPart == 0));
+    }
+
+    public float getRealPart() {
+        return this.realPart;
+    }
+
+    public float getImaginaryPart() {
+        return this.imaginaryPart;
+    }
+
+    public void setRealPart(float a) {
+        this.realPart = a;
+    }
+
+    public void setImaginaryPart(float b) {
+        this.imaginaryPart = b;
+    }
+
+    public Complex sum(Complex c) {
+        return new Complex(this.realPart + c.getRealPart(), this.imaginaryPart + c.getImaginaryPart());
+    }
+
+    public Complex product(Complex c) {
+        // TODO: change code
+        return new Complex();
+    }
+
+    public Complex inverse() throws IllegalArgumentException {
+        if (isZero()) throw new IllegalArgumentException();
+        float square_norm = (this.realPart * this.realPart + this.imaginaryPart * this.imaginaryPart);
+        return new Complex(this.realPart / square_norm, -this.imaginaryPart / square_norm);
+    }
+
+    @Override
+    public String toString() {
+        return realPart + "+i" + imaginaryPart;
+    }
+
+    public static void infinite()
+    {
+        // TODO: add code
+    }
+}
---
+++ src/test/java/complex/ComplexTest.java
@@ -0,0 +1,167 @@
+package complex;
+
+/* nécessaire pour les timeout */
+
+/* importer les annotations utilisées */
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.within;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+/**
+ * Test class for the complex class.
+ */
+
+class ComplexTest {
+  private static final float EPSILON = 0.000001F;
+
+  @BeforeAll
+  static void setUpBeforeClass() throws Exception {
+    // TODO: add message
+  }
+
+  @AfterAll
+  static void tearDownAfterClass() throws Exception {
+    // TODO: add message
+  }
+
+  @BeforeEach
+  void setUp() throws Exception {
+    // TODO: add message
+  }
+
+  @AfterEach
+  void tearDown() throws Exception {
+    // TODO: add message
+  }
+
+
+  @Test
+  void testGetterImaginary() {
+
+    float expected = 2.0F;
+    Complex z = new Complex(1.0F, 2.0f);
+    assertThat(z.getImaginaryPart()).as("problem on getter imaginary")
+            .isCloseTo(expected, within(EPSILON));
+
+  }
+
+  @Test
+  void testGetterReal() {
+
+    float expected = 1.0F;
+    Complex z = new Complex(1.0F, 2.0f);
+
+    assertThat(z.getRealPart()).as("problem on getter imaginary")
+            .isCloseTo(expected, within(EPSILON));
+
+  }
+
+  @Test
+  void testSetterImaginary() {
+
+    float expected = 3.0F;
+    Complex z = new Complex();
+    z.setImaginaryPart(expected);
+    assertThat(z.getImaginaryPart()).as("problem on setter imaginary")
+            .isCloseTo(expected, within(EPSILON));
+
+  }
+
+  @Test
+  void testSetterReal() {
+
+    float expected = 3.0F;
+    Complex z = new Complex();
+    z.setRealPart(expected);
+    assertThat(z.getRealPart()).as("problem on getter imaginary")
+            .isCloseTo(expected, within(EPSILON));
+  }
+
+  @Test
+  void testZeroTrue() {
+    Complex z = new Complex(0.0F, 0.0F);
+    assertThat(z.isZero()).as("problem with isZero on Zero Complex number")
+            .isTrue();
+  }
+
+  @Test
+  void testZeroFalse() {
+    Complex z = new Complex(1.0F, 1.0F);
+    assertThat(z.isZero()).as("problem with isZero on non Zero Complex number")
+            .isFalse();
+  }
+
+  @Test
+  void testSumReal() {
+    Complex z1 = new Complex(1.0F, 2.0F);
+    Complex z2 = new Complex(3.0F, 4.0F);
+
+    float expected = 1.0F + 3.0F;
+
+    Complex z = z1.sum(z2);
+    assertThat(z.getRealPart()).as("problem with Real part of Sum")
+            .isCloseTo(expected, within(EPSILON));
+  }
+
+  @Test
+  void testSumImaginary() {
+    Complex z1 = new Complex(1.0F, 2.0F);
+    Complex z2 = new Complex(3.0F, 4.0F);
+
+    float expected = 2.0F + 4.0F;
+    Complex z = z1.sum(z2);
+    assertThat(z.getImaginaryPart()).as("problem with Imaginary part of Sum")
+            .isCloseTo(expected, within(EPSILON));
+  }
+
+
+  // TODO: activate test
+  @Disabled
+  void testProductReal() {
+    Complex z1 = new Complex(1.0F, 2.0F);
+    Complex z2 = new Complex(3.0F, 4.0F);
+
+    float expected = 1.0F * 3.0F - 2.0F * 4.0F;
+
+    Complex z = z1.product(z2);
+    assertThat(z.getRealPart()).as("problem with Real part of Product")
+            .isCloseTo(expected, within(EPSILON));
+
+  }
+
+  // TODO: activate test
+  @Disabled
+  void testProductImaginary() {
+    Complex z1 = new Complex(1.0F, 2.0F);
+    Complex z2 = new Complex(3.0F, 4.0F);
+
+    float expected = 1.0F * 4.0F + 2.0F * 3.0F;
+
+    Complex z = z1.product(z2);
+    assertThat(z.getImaginaryPart()).as("problem with Imaginary part of Product")
+            .isCloseTo(expected, within(EPSILON));
+
+
+  }
+
+
+  // TODO: Write a test that fails if computation with infinite takes too long
+
+  @Disabled
+  final void testTimeoutInfinite() {
+    Complex.infinite();
+    fail("NYI");
+  }
+
+
+}
