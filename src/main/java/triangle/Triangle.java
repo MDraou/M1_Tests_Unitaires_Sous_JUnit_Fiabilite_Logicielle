a608c9f3102d94b0cb6073d6e0012229768aa761
draou <meryem.draou@etu.univ-amu.fr>
2023-01-06 14:19:31 +0100

    premier constructeur et exception levée

--- src/main/java/triangle/Triangle.java
+++ src/main/java/triangle/Triangle.java
@@ -9,12 +9,20 @@ public class Triangle {
         public Triangle() {
         }
 
+        /**
+        * la somme de 2 côtés d'un triangle doit être supérieure au 3ème côté pour être un traingle
+        */
         public Triangle(float a, float b, float c) {
 
             this.edgeLengthA = a;
             this.edgeLengthB = b;
             this.edgeLengthC = c;
 
+            if(a + b > c) {
+                throw new IllegalArgumentException("Not a triangle.");
+            }
         }
+
+
 
 }
