f6234df6d4fa08d6726331983176a1dec8bbe504
draou <meryem.draou@etu.univ-amu.fr>
2023-01-06 13:51:22 +0100

    3.1. début avec création des attributs et constructeur

--- src/main/java/triangle/Triangle.java
+++ src/main/java/triangle/Triangle.java
@@ -1,4 +1,20 @@
 package triangle;
 
 public class Triangle {
+
+        private float edgeLengthA = 0;
+        private float edgeLengthB = 0;
+        private float edgeLengthC = 0;
+
+        public Triangle() {
+        }
+
+        public Triangle(float a, float b, float c) {
+
+            this.edgeLengthA = a;
+            this.edgeLengthB = b;
+            this.edgeLengthC = c;
+
+        }
+
 }
