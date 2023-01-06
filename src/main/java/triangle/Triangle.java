6845f996d78ce7e66e921f0cd66c5d4a3a05f1e9
draou <meryem.draou@etu.univ-amu.fr>
2023-01-06 14:52:42 +0100

    deuxième constructeur et getters

--- src/main/java/triangle/Triangle.java
+++ src/main/java/triangle/Triangle.java
@@ -1,13 +1,14 @@
 package triangle;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class Triangle {
 
         private float edgeLengthA = 0;
         private float edgeLengthB = 0;
         private float edgeLengthC = 0;
-
-        public Triangle() {
-        }
 
         /**
         * la somme de 2 côtés d'un triangle doit être supérieure au 3ème côté pour être un traingle
@@ -23,6 +24,27 @@ public class Triangle {
             }
         }
 
+        public Triangle(File inputTriangleData) {
 
+            try {
+                FileWriter writer = new FileWriter(inputTriangleData, true);
+                writer.write("Ligne 1\n");
+                writer.write("Ligne 2\n");
+                writer.write("Ligne 3\n");
+                writer.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        public float getEdgeLengthA(){
+            return edgeLengthA;
+        }
+        public double getEdgeLengthB(){
+            return edgeLengthB;
+        }
+        public double getEdgeLengthC(){
+            return edgeLengthC;
+        }
 
 }
