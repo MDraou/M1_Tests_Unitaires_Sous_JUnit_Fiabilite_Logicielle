ded9c816dad9995b367b94db9d2feae9969c8959
draou <meryem.draou@etu.univ-amu.fr>
2023-01-08 16:28:02 +0100

    création du package search et de la classe search_ArrayTest

---
+++ src/test/java/search/search_ArrayTest.java
@@ -0,0 +1,14 @@
+package search;
+
+import com.company.*;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+
+class search_ArrayTest {
+
+}
