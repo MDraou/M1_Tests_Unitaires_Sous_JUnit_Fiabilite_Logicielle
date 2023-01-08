a70b6858488f8e5918faf395b383c9a948c6c4f3
draou <meryem.draou@etu.univ-amu.fr>
2023-01-08 17:05:38 +0100

    test pour la classe search_Array_Class_3 et la classe search_Array_Class_4

--- src/test/java/search/search_ArrayTest.java
+++ src/test/java/search/search_ArrayTest.java
@@ -27,23 +27,44 @@ class search_ArrayTest {
     @Test
     void search_Array_Class_1_Test() {
 
-        int expected = 5;
+        int expected = 0;
         search_Array_Class_1 objet = new search_Array_Class_1();
         int[] t = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        objet.search(t, 5);
-        assertEquals(expected,5);
+        objet.search(t, 0);
+        assertEquals(expected,0);
         assertNotEquals(expected, 1);
 
     }
 
     @Test
     void search_Array_Class_2_Test(){
-        int expected = 0;
+        int expected = 1;
         search_Array_Class_2 objet = new search_Array_Class_2();
         int[] t = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-        objet.search(t,0);
-        assertEquals(expected, 0);
-        assertNotEquals(expected, 1);
+        objet.search(t,1);
+        assertEquals(expected, 1);
+        assertNotEquals(expected, 2);
+    }
+
+    @Test
+    void search_Array_Class_3_Test(){
+        int expected = 2;
+        search_Array_Class_2 objet = new search_Array_Class_2();
+        int[] t = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        objet.search(t,2);
+        assertEquals(expected, 2);
+        assertNotEquals(expected, 3);
+    }
+
+
+    @Test
+    void search_Array_Class_4_Test(){
+        int expected = 3;
+        search_Array_Class_2 objet = new search_Array_Class_2();
+        int[] t = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        objet.search(t,3);
+        assertEquals(expected, 3);
+        assertNotEquals(expected, 4);
     }
 
 
