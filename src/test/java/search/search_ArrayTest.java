5a9039b9ff8aeedee75e58c7a4b84068ffec5297
draou <meryem.draou@etu.univ-amu.fr>
2023-01-08 16:51:41 +0100

    test pour la classe search_Array_Class_1

--- src/test/java/search/search_ArrayTest.java
+++ src/test/java/search/search_ArrayTest.java
@@ -11,7 +11,27 @@ import org.junit.jupiter.api.Test;
  * Test class for the search_Array Classes.
  */
 
+// à continuer
 class search_ArrayTest {
+
+    private int[] tab;
+
+    private int elt;
+
+
+    /**
+     * test pour la méthode search() de la classe search_Array_Class_1.
+     */
+    @Test
+    void search_Array_Class_1_Test() {
+
+        int expected = 5;
+        search_Array_Class_1 objet = new search_Array_Class_1();
+        int[] t = new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+        objet.search(t, 5);
+        assertEquals(expected,5);
+
+    }
 
 
 }
