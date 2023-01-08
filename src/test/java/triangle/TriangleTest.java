abd172d8d6f58c543e6f0668122144d10aacc180
draou <meryem.draou@etu.univ-amu.fr>
2023-01-08 15:21:23 +0100

    ajout des tests pour les setters

--- src/test/java/triangle/TriangleTest.java
+++ src/test/java/triangle/TriangleTest.java
@@ -16,6 +16,10 @@ class TriangleTest {
     private Triangle triangle2;
     private Triangle triangle3;
 
+
+    /**
+     *  ajout des tests pour les getters
+     */
     @Test
     void testGetterEdgeLengthA(){
         float expected = 1.0F;
@@ -36,6 +40,34 @@ class TriangleTest {
         triangle1 = new Triangle(1.0F,3.0F,2.0F);
         assertEquals(expected, triangle1.getEdgeLengthC());
     }
+
+    /**
+     *  ajout des tests pour les setters
+     */
+    @Test
+    void SetEdgeLengthA(){
+        float expected = 1.0F;
+        triangle1 = new Triangle(1.0F, 3.0F, 2.0F);
+        triangle1.setEdgeLengthA(expected);
+        assertEquals(expected, 1.0F);
+    }
+
+    @Test
+    void SetEdgeLengthB(){
+        float expected = 3.0F;
+        triangle1 = new Triangle(1.0F,3.0F,2.0F);
+        triangle1.setEdgeLengthB(expected);
+        assertEquals(expected, 3.0F);
+    }
+
+    @Test
+    void SetEdgeLengthC(){
+        float expected = 2.0F;
+        triangle1 = new Triangle(1.0F,3.0F,2.0F);
+        triangle1.setEdgeLengthB(2.0F);
+        assertEquals(expected, 2.0F);
+    }
+
 
     /**
      *  ajout des tests pour la méthode TriangleType type()
